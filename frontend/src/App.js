d4f90c78f19622a523fa6d9e29410caa3894d06d
Amal Abeygunawardana <amalabey@gmail.com>
2021-07-31 18:39:34 +1000

    needs fixing..:(

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
+import 'date-fns';
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 import { useEffect } from 'react/cjs/react.production.min';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker
+} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 
 function App() {
   const [alarms, setAlarms] = useState(null);
+  const [selectedTime, setAlarmTime] = useState(null);
   const apiUrl = "/api/v1/alarms";
 
   const fetchData = async () => {
@@ -13,12 +20,30 @@ function App() {
     setAlarms(response.data) 
   }
 
+  const handleDateChange = (date) => {
+    setAlarmTime(date);
+  };
+
   useEffect(() => {
     fetchData();
   },[]);
 
   return (
     <div className="App">
+      <div className="setAlarm">
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardTimePicker
+            margin="normal"
+            id="time-picker"
+            label="Time picker"
+            value={selectedTime}
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change time',
+            }}
+          />
+        </MuiPickersUtilsProvider>
+      </div>
       <div className="alarms">
         {alarms &&
           alarms.map((alarm, index) => {
