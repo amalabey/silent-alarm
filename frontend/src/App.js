c209ff09da94e8302d6f95e70e9ac5d2af53c26d
Amal Abeygunawardana <amalabey@gmail.com>
2021-07-31 18:07:12 +1000

    data displayed on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [alarms, setAlarms] = useState(null);
+  const apiUrl = "/api/v1/alarms";
+
+  const fetchData = async () => {
+    const response = await axios.get(apiUrl)
+    setAlarms(response.data) 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button className="fetch-button" onClick={fetchData}>
+          Fetch Data
+        </button>
+      </div>
+      <div className="alarms">
+        {alarms &&
+          alarms.map((alarm, index) => {
+            return (
+              <div className="alarm" key={index}>
+                <h3>{alarm.time}</h3>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
